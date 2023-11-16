9bc67cfa9808fd1d23805ffce5452df403ddc869
Devon O'Shaughnessy <devonosh@gmail.com>
2023-11-15 21:55:26 -0600

    Update api-service.service.ts

--- docker-example-ui/src/app/api-service.service.ts
+++ docker-example-ui/src/app/api-service.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { MealPlan } from './model';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,5 +12,22 @@ export class APIService {
 
   public getCurrentTime() {
     return this.http.get('/currentTime');
+  }
+
+  public getMeals(): Observable<Array<MealPlan>> {
+    return this.http.get<Array<MealPlan>>('/mealPlans');
+  }
+
+  public createMeal(dow: string, meal: string, mealName: string) {
+    let mealPlan = {
+        dow,
+        meal,
+        mealName
+    }
+    return this.http.post('/mealPlans', mealPlan);
+  }
+
+  public deleteMeal(id: number) {
+    return this.http.delete('/mealPlans/'+id);
   }
 }
